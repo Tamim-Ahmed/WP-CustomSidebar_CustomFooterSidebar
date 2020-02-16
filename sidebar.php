34ef05c467aacaa6d62552373f773a842b87551a
Tamim Ahmed <rtamim04@gmail.com>
2020-02-16 23:54:26 +0600

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+/* Sidebar add STARTS ( Custom Wordpress Sidebar or Footer Sidebar by Tamim Ahmed ) */
+
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar") ) : ?>
+
+/* Sidebar add ends ( Custom Wordpress Sidebar or Footer Sidebar by Tamim Ahmed ) */
